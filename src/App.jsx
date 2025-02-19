806da0ad6dc0141d494d1c7a36b34a242ab481b1
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2025-02-18 21:11:55 -0500

    display selected puppy id

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,25 @@ import "./App.css";
 import { puppyList } from "./data.js";
 
 function App() {
-  console.log(puppyList);
   const [puppies, setPuppies] = useState(puppyList);
-  console.log("puppies: ", puppies);
+  const [featPupId, setFeatPupId] = useState(null);
 
   return (
     <div className="App">
+      {featPupId && <p>Pup Id: {featPupId}</p>}
+
       <h1>Puppy List</h1>
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
     </div>
   );
