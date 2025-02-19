ec8339385038bce626f9d2c540228a8c15aec44b
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2025-02-18 20:59:50 -0500

    render puppy list

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { puppyList } from "./data.js";
 
 function App() {
-  const [count, setCount] = useState(0);
+  console.log(puppyList);
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppies: ", puppies);
 
   return (
-    <>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </>
+    <div className="App">
+      <h1>Puppy List</h1>
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
   );
 }
 
